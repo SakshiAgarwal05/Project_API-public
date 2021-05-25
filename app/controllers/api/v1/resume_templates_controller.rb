62cd00cdd016f23017c667908b4917b50b26e7f4
SakshiAgarwal05 <sakshia@crowdstaffing.com>
2021-05-25 18:46:22 +0530

    deleting files

--- app/controllers/api/v1/resume_templates_controller.rb
+++
@@ -1,9 +0,0 @@
-# ResumeTemplatesController
-class Api::V1:: ResumeTemplatesController < ApplicationController
-  # List Resume templates names
-  # ====URL
-  #   /resume_templates [GET]
-  def index
-    @resume_templates = ResumeTemplate.order(name: :asc)
-  end
-end
--- app/controllers/api/v1/templates_controller.rb
+++
@@ -1,9 +0,0 @@
-# TemplatesController
-class Api::V1:: TemplatesController < ApplicationController
-  # List all templates names
-  # ====URL
-  #   /templates [GET]
-  def index
-    @templates = Template.order(name: :asc)
-  end
-end
